f04f12d15d4818b8eb9337c2bf096180c645047f
Almar <gomezalmar26@gmail.com>
2025-07-04 18:11:26 +0800

    Added nuxt auth plugins

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="d-flex align-center justify-center" style="height: 100vh">
-    <v-card width="500" class="pa-8">
-      <h1 class="text-center my-6">Login Page</h1>
+    <v-card width="400" class="card-outline px-8 py-10 text-center">
+      <h2 class="text-center my-6 hr-sect mb-10">Login Page</h2>
       <v-form>
         <v-text-field
-          label="Email"
+          label="Username"
           outlined
-          prepend-inner-icon="mdi-email"
+          rounded
+          prepend-inner-icon="mdi-account"
         ></v-text-field>
         <v-text-field
           label="Password"
           outlined
+          rounded
           type="password"
           prepend-inner-icon="mdi-lock"
         ></v-text-field>
 
-        <v-btn color="primary" block>Login</v-btn>
+        <v-btn color="primary" class="text-capitalize" rounded block>Login</v-btn>
+        <div class="hr-sect my-5">OR</div>
+        <v-btn color="error" class="text-capitalize" rounded block><v-icon left>mdi-google</v-icon> Sign in with google</v-btn>
       </v-form>
     </v-card>
   </div>
@@ -25,3 +29,27 @@ export default {
   layout: "auth",
 };
 </script>
+<style scoped>
+.hr-sect {
+    display: flex;
+    flex-basis: 100%;
+    align-items: center;
+    /* color: rgba(0, 0, 0, 0.9); */
+    margin: 8px 0;
+}
+
+.hr-sect::before,
+.hr-sect::after {
+    content: "";
+    flex-grow: 1;
+    background: rgba(255, 255, 255, 0.15);
+    height: 1px;
+    font-size: 0;
+    line-height: 0;
+    margin: 0 8px;
+}
+
+.card-outline {
+  border-radius: 30px;
+}
+</style>
